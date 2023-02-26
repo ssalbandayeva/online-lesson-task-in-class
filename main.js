865f01596c24ed768fd4fa882db8c5410ad9b3a8
ssalbandayeva <samrasa@code.edu.az>
2023-02-26 20:48:30 +0400

    task in class of online lesson

---
+++ main.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector(".btn");
+let menu_bar = document.querySelector(".menu_bar");
+btn.addEventListener("click", function () {
+  menu_bar.classList.toggle("active");
+});
